d163a7d432d5d1a8bac929c02c36ba6f5a378095
lucky-promethean <62021162+lucky-promethean@users.noreply.github.com>
2020-04-03 10:37:37 -0400

    Update tediusfraction.py

--- tediusfraction.py
+++ tediusfraction.py
@@ -3,15 +3,20 @@
 ## GITHUB.COM/LUCKY-PROMETHEAN
 ##THIS PROGRAM ALLOWS YOU TO DIVIDE ONE NUMBER BY MULTIPLE DIFFERENT NUMBERS REPEATEDLY QUICKLY
 
-V = float(input("Enter number that will stay constant , 0 for default [10] "));
+V = float(input("Enter number that will stay constant "))
 if V==0: V=10;
 i=1
 result=0
+torb=str(input("is the constant number to appear at the [t]op or [b]ottom"))
+checker=bool;
+if torb=='t': checker=bool(1)
+
 finalincrement=int(input("how many repitions "));
 while i<finalincrement+1:
-        num = float(input("Enter number "));
-        print ("division number", i)
-        print(V/num)
-        i=i+1
-
+    num = float(input("Enter number "));
+    if checker==(1): result=V/num
+    else: result=num/V
+    print ("division number", i);
+    print(result)
+i=i+1
 
