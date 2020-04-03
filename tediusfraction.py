d4d4aa7b72c79792aeb1b31e89392b0a20d572b5
lucky-promethean <62021162+lucky-promethean@users.noreply.github.com>
2020-04-03 08:03:29 -0400

    TEDIUSFRACTION.PY

---
+++ tediusfraction.py
@@ -0,0 +1,14 @@
+##INTELLECTUAL PROPERTY OF LUCKY GORAYA AND PROMETHEAN ENTERPRISES
+##ANYONE WHO REMOVES THIS HEADER OR TRIES TO SELL THIS CODE WILL BE DEALT WITH
+## GITHUB.COM/LUCKY-PROMETHEAN
+
+VL=10.00000
+i=1
+result=0
+finalincrement=int(input("how many repitions "));
+while i<finalincrement+1:
+        num = float(input("Enter number "));
+        print ("division number", i)
+        print(VL/num)
+        i=i+1
+
