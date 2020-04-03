c8eb84009ddc48adcba647fb4c1ed1c21a50c270
lucky-promethean <62021162+lucky-promethean@users.noreply.github.com>
2020-04-03 09:10:07 -0400

    Update tediusfraction.py

--- tediusfraction.py
+++ tediusfraction.py
@@ -1,14 +1,17 @@
 ##INTELLECTUAL PROPERTY OF LUCKY GORAYA AND PROMETHEAN ENTERPRISES
 ##ANYONE WHO REMOVES THIS HEADER OR TRIES TO SELL THIS CODE WILL BE DEALT WITH
 ## GITHUB.COM/LUCKY-PROMETHEAN
+##THIS PROGRAM ALLOWS YOU TO DIVIDE ONE NUMBER BY MULTIPLE DIFFERENT NUMBERS REPEATEDLY QUICKLY
 
-VL=10.00000
+V = float(input("Enter number that will stay constant , 0 for default [10] "));
+if V==0: V=10;
 i=1
 result=0
 finalincrement=int(input("how many repitions "));
 while i<finalincrement+1:
         num = float(input("Enter number "));
         print ("division number", i)
-        print(VL/num)
+        print(V/num)
         i=i+1
+
 
